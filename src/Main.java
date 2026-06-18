54b89a8eec5a9071fcd0f1547b4ff07527d7a3cd
TANAN101 <abdibezu5@gmail.com>
2026-06-18 15:19:18 +0300

    added function on main that displays totall gpa and highest gpa

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String file_pa = "student_info.txt";
-
+        String file_patxt = "student_info.txt";
+        String file_padat="student_info.dat";
         Scanner input = new Scanner(System.in);
         System.out.println("1:Add student");
         System.out.println("2:Srearch for student");
@@ -27,13 +27,22 @@ public class Main {
 
             try {
 
-                PrintWriter pw = new PrintWriter(new FileWriter(file_pa, true));
+                PrintWriter pw = new PrintWriter(new FileWriter(file_patxt, true));
 
                 pw.println(add.student_ID + " " + add.Name + " " + add.Department + " " + add.GPA);
                 pw.close();
+                DataOutputStream q = new DataOutputStream(new FileOutputStream(file_padat,true));
+
+                q.writeInt(add.student_ID );
+                q.writeUTF(add.Name );
+                q.writeUTF(add.Department );
+                q.writeDouble(add.GPA );
+                q.close();
+
             } catch (IOException e) {
                 System.out.println(e);
             }
+
 
         }
 ///search file
@@ -43,7 +52,7 @@ public class Main {
 
             try {
 
-                Scanner look = new Scanner(new File(file_pa));
+                Scanner look = new Scanner(new File(file_patxt));
 
             while(look.hasNext()){
                 String line=look.nextLine();
@@ -62,41 +71,65 @@ public class Main {
 
         ///update file
 
-        if (a==3){
+        if (a == 3) {
             try {
-                System.out.println("Enter the name you want to update:  ");
-                String whom =input.next();
-                System.out.println("Enter the name you want to update it to:  ");
-                String To =input.next();
-                Scanner update = new Scanner(new File(file_pa));
-                ArrayList<String> up_name=new ArrayList<>();
-                while (update.hasNext()){
+                System.out.println("Enter the name you want to update: ");
+                String whom = input.next();
+                System.out.println("Enter the name you want to update it to: ");
+                String To = input.next();
+
+                // --- Update TXT ---
+                Scanner update = new Scanner(new File(file_patxt));
+                ArrayList<String> up_name = new ArrayList<>();
+                while (update.hasNext()) {
                     up_name.add(update.nextLine());
                 }
                 update.close();
-                for(int i=0;i<up_name.size();i++){
-                    if (up_name.get(i).contains(whom)){
-                        up_name.set(i,up_name.get(i).replace(whom,To));
 
+                for (int i = 0; i < up_name.size(); i++) {
+                    if (up_name.get(i).contains(whom)) {
+                        up_name.set(i, up_name.get(i).replace(whom, To));
                     }
-                    PrintWriter pupdate = new PrintWriter(new FileWriter(file_pa));
-                   for (String s:up_name){
-                       pupdate.println(s);
-                   }
-                    pupdate.close();
                 }
+                // Move PrintWriter OUTSIDE the loop
+                PrintWriter pupdate = new PrintWriter(new FileWriter(file_patxt));
+                for (String s : up_name) {
+                    pupdate.println(s);
+                }
+                pupdate.close();
 
+                // --- Also update DAT ---
+                DataInputStream in = new DataInputStream(new FileInputStream(file_padat));
+                ArrayList<student> students = new ArrayList<>();
 
+                try {
+                    while (true) {
+                        int id      = in.readInt();
+                        String name = in.readUTF();
+                        String dep  = in.readUTF();
+                        double gpa  = in.readDouble();
+                        if (name.equals(whom)) name = To; // apply update
+                        students.add(new student(id, name, dep, gpa));
+                    }
+                } catch (EOFException e) { }
+                in.close();
 
+                DataOutputStream out = new DataOutputStream(new FileOutputStream(file_padat)); // overwrite
+                for (student s : students) {
+                    out.writeInt(s.student_ID);
+                    out.writeUTF(s.Name);
+                    out.writeUTF(s.Department);
+                    out.writeDouble(s.GPA);
+                }
+                out.close();
 
-            }catch (IOException e){System.out.println(e);}
-
+            } catch (IOException e) { System.out.println(e); }
         }
 ///Remove student
         if (a==4){
             System.out.println("Who do you want to delete :  ");
             String delete = input.next();
-           try{  Scanner remove = new Scanner(new File(file_pa));
+           try{  Scanner remove = new Scanner(new File(file_patxt));
             ArrayList<String> rm_name=new ArrayList<>();
             while (remove.hasNext()){
                 rm_name.add(remove.nextLine());}
@@ -105,7 +138,7 @@ public class Main {
                     rm_name.remove(rm_name.get(i));
                 }
             }
-               PrintWriter pupdate = new PrintWriter(new FileWriter(file_pa));
+               PrintWriter pupdate = new PrintWriter(new FileWriter(file_patxt));
             for (String l:rm_name){
                 pupdate.println(l);
             }
@@ -122,6 +155,38 @@ public class Main {
         if (a==5){
             displayStudents show = new displayStudents();
             show.display();
+        }
+        if (a == 6) {
+            double count =0;
+            try{
+                Scanner total = new Scanner(new File(file_patxt));
+
+                while(total.hasNextLine()){
+                    String[] liss=total.nextLine().split(" ");
+                   Double sum =Double.parseDouble(liss[3]);
+                   count=count+sum;
+
+                }
+                System.out.println(count);
+
+            }catch (FileNotFoundException e){}
+
+        }
+        if (a==7){
+            try{
+                double finall=0;
+                double set =0;
+                Scanner higest = new Scanner(new File(file_patxt));
+                while(higest.hasNextLine()){
+                    String[] high=higest.nextLine().split(" ");
+                    double highh =Double.parseDouble(high[3]);
+                    if(set<=highh){
+                         set=highh;
+                    }
+                }
+                System.out.println(set);
+
+            }catch (FileNotFoundException e){}
         }
     }
 }
